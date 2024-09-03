71749086b16dcd2934a8548a89b6f278cde9d7b8
lokeshjaat-9518 <kingdhull9518@gmail.com>
2024-09-03 21:23:03 +0530

    Kala.py

--- Kala.py
+++ Kala.py
@@ -4,8 +4,8 @@ import logging
 from aiogram import Bot
 import asyncio
 
-API_TOKEN = '7400337646:AAG58cKuTq4gJcp0Q7n7sfPFJt9yW5cpsAI'
-ADMIN_ID = '6862918172'
+API_TOKEN = '7319682073:AAHopabS6i6La2yio9hNfRfJ6a3IxRgdDO0'
+ADMIN_ID = '6269073069'
 MAX_RESTARTS = 5
 RESTART_PERIOD = 60  # Seconds
 
